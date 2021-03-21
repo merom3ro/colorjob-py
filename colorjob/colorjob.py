1c13829ecf77dd515bc4b4a961d4b4986f72b68a
Roman Sizonenko <me.rom3ro@gmail.com>
2021-03-21 23:09:54 +0800

    #1: overtaking the proxy wall of servers (sending file directly)

--- colorjob/colorjob.py
+++ colorjob/colorjob.py
@@ -2,18 +2,23 @@ import os
 
 from django.http import HttpResponse, JsonResponse
 from colorthief import ColorThief
-from urllib import request
 import itertools
+
+from colorjob.forms import UploadFileForm
 
 cont = itertools.count()
 
 def index(_request):
+    print(_request)
+    print(_request.FILES)
     print(_request.headers)
-    fname = str.format("img{}.png", next(cont))
-    request.urlretrieve(
-        _request.headers['Imgurl'],
-        fname)
-    thief = ColorThief(fname)
-    palette = thief.get_palette(color_count=6, quality=6)
-    os.remove(fname)
-    return JsonResponse({'palette': palette})
+
+    if _request.method == 'POST':
+        form = UploadFileForm(_request.POST, _request.FILES)
+        if form.is_valid():
+            thief = ColorThief(_request.FILES['file'])
+            palette = thief.get_palette(color_count=6, quality=6)
+            print(palette)
+            return JsonResponse({'palette': palette})
+    return HttpResponse('non-valid request')
+
